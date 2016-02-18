d3db66cc5eb0b05afce1b7988000562e232dd71e
Blair Peterson and Megan Fayer <student@epicodus.com>
2016-02-18 14:49:22 -0800

    card, deck, and shuffle

---
+++ src/main/java/Card.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Card {
+  private String mSuit;
+  private String mValue;
+
+  public Card(String suit, String value) {
+    mSuit = suit;
+    mValue = value;
+  }
+
+  public String getSuit() {
+    return mSuit;
+  }
+
+  public String getValue() {
+    return mValue;
+  }
+
+  public String name() {
+    return String.format("%s of %s", mValue, mSuit);
+  }
+}
